8bdd9b28a30ad882f091b3bde5abd6f7d94fd046
santiago <santiagonutuc@gmail.com>
2026-05-27 01:27:52 -0500

    Primer commit: Proyecto de SIGE completo

---
+++ main.py
@@ -0,0 +1,9 @@
+from interfaz.app_principal import App
+
+if __name__ == "__main__":
+    # Creamos la instancia de la aplicación
+    app = App()
+    # Iniciamos el bucle principal
+    app.mainloop()
+
+    
